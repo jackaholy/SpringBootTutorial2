7fc61035a845f74ac2aa43641b271e772b780842
Jack Holy <jholy@carroll.edu>
2024-08-28 11:17:48 -0600

    Modify controller classes

--- src/main/java/edu/carroll/cs341/web/controller/LoginController.java
+++ src/main/java/edu/carroll/cs341/web/controller/LoginController.java
@@ -1,14 +1,27 @@
 package edu.carroll.cs341.web.controller;
 
+import edu.carroll.cs341.service.LoginService;
 import edu.carroll.cs341.web.form.LoginForm;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
+    private final LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+//    private static final String validUser = "cs341user";
+//    private static final String validPass = "supersecret";
+
     @GetMapping("/login")
     public String loginGet(Model model) {
         model.addAttribute("loginForm", new LoginForm());
@@ -16,13 +29,21 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String loginPost(@ModelAttribute LoginForm loginForm) {
-        System.out.println("User '" + loginForm.getUsername() + "' attempted login");
+    public String loginPost(@Valid @ModelAttribute LoginForm loginForm, BindingResult result, RedirectAttributes attrs) {
+        if (result.hasErrors()) {
+            return "login";
+        }
+        if (!(loginService.validateUser(loginForm))) {
+            result.addError(new ObjectError("globalError", "Username and password do not match known users"));
+            return "login";
+        }
+        attrs.addAttribute("username", loginForm.getUsername());
         return "redirect:/loginSuccess";
     }
 
     @GetMapping("/loginSuccess")
-    public String loginSuccess() {
+    public String loginSuccess(String username, Model model) {
+        model.addAttribute("username", username);
         return "loginSuccess";
     }
 
--- src/main/java/edu/carroll/cs341/web/form/LoginForm.java
+++ src/main/java/edu/carroll/cs341/web/form/LoginForm.java
@@ -1,7 +1,19 @@
 package edu.carroll.cs341.web.form;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Size;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class LoginForm {
+    @NotNull
+    @Size(min = 6, message = "Username must be at least 6 characters long")
     private String username;
+
+    @NotNull
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 
     public String getUsername() {
@@ -18,5 +30,14 @@ public class LoginForm {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @PostMapping("/login")
+    public String loginPost(@Valid @ModelAttribute LoginForm loginForm, BindingResult result) {
+        System.out.println("User '" + loginForm.getUsername() + "' attempted login");
+        if (result.hasErrors()) {
+            return "login";
+        }
+        return "redirect:/loginSuccess";
     }
 }
